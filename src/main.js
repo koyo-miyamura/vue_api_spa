bdc19114cc08dbb5418e2e214b8dfaae115ca06f
koyo <nexus9071@gmail.com>
2018-09-27 00:49:30 +0900

    コンポーネントをより細かく分ける（v-modelまわりの考慮も行う）

--- src/main.js
+++ src/main.js
@@ -1,37 +1,64 @@
-Vue.component('query-serarch-content', {
+Vue.component('query-search-content', {
+  props: ['errored', 'keys', 'loading', 'query', 'items', 'articles'],
+  template: `
+    <div class="container">
+      <h1>Vue.js with QiitaAPI</h1>
+
+      <input :value="query" @keyup.enter="$emit('query-input', $event.target.value)" placeholder="検索ワードを入れてね">
+      <input :value="items" @keyup.enter="$emit('items-input', $event.target.value)" placeholder="何件表示しますか？">
+
+      <section v-if="errored">
+        <p>エラーが発生したよ＞＜</p>
+      </section>
+
+      <section v-else>
+        <query-search-result :keys="keys" :loading="loading" :query="query" :articles="articles"></query-search-result>
+      </section>
+    </div>
+  `
+})
+
+Vue.component('query-search-result', {
   props: ['keys', 'loading', 'query', 'articles'],
   template: `
     <div>
       <div v-if="loading">Loading...</div>
       <div class="panel panel-info table-responsive">
         <div class="panel-heading"><b> {{ query }} </b>のQiita記事！</div>
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th v-for="key in keys">
-                {{ key }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="article in articles" :key="articles.id">
-              <td v-for="key in keys">
-                <span v-if="key === 'title'">
-                  <a :href="article.url">{{ article[key] }}</a>
-                </span>
-                <span v-else="key === 'title'">
-                    {{ article[key] }}
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <content-table :keys="keys" :articles="articles"></content-table>
       </div>
     </div>
   `
 })
 
-new Vue({
+Vue.component('content-table', {
+  props: ['keys', 'articles'],
+  template: `
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th v-for="key in keys">
+            {{ key }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="article in articles" :key="articles.id">
+          <td v-for="key in keys">
+            <span v-if="key === 'title'">
+              <a :href="article.url">{{ article[key] }}</a>
+            </span>
+            <span v-else="key === 'title'">
+                {{ article[key] }}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  `
+})
+
+var vm = new Vue({
   el: '#app',
   data: function() {
     return {
