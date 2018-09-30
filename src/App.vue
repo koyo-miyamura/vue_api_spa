8c58e0d1e6161eee27e62a89801bbe7f00706840
koyo <nexus9071@gmail.com>
2018-09-30 21:23:19 +0900

    bootstrapを除いて移行完了

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/">Query search</router-link>
     </div>
     <router-view/>
   </div>
@@ -13,7 +13,6 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
---
+++ src/components/QuerySearchResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div class="panel panel-info table-responsive">
+      <div class="panel-heading"><b> {{ query }} </b>のQiita記事！</div>
+      <content-table :keys="keys" :articles="articles"></content-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContentTable from '@/components/ContentTable.vue'
+export default {
+  components: {
+    ContentTable
+  },
+  props: ['keys', 'loading', 'query', 'articles']
+}
+</script>
