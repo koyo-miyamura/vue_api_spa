44f54449d7704c8adbf0d7b47b08100564f4d2f7
koyo <nexus9071@gmail.com>
2018-10-04 23:09:31 +0900

    QiitaAPII認証して1000req/hできるように

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -24,6 +24,8 @@
 <script>
 import QuerySearchResult from '@/components/QuerySearchResult.vue'
 import axios from 'axios'
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.VUE_APP_QIITA_API_KEY}`
+axios.defaults.headers.common['Content-Type'] = `application/json`
 
 export default {
   name: 'index',
