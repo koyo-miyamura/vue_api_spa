aa3213485616930b3873cd29ef1384afd66ee3e9
koyo <nexus9071@gmail.com>
2018-10-07 02:29:33 +0900

    googleアナリティクス導入

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import ElementUI from 'element-ui'
 import './styles.scss'
 import locale from 'element-ui/lib/locale/lang/en'
 
+import VueAnalytics from 'vue-analytics'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -16,6 +18,11 @@ library.add(faGithub, faTwitter)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(ElementUI, { locale })
+
+Vue.use(VueAnalytics, {
+  id: 'UA-120919612-2',
+  router
+})
 
 Vue.config.productionTip = false
 
